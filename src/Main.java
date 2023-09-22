a27d56843ce43cea07f9b292c5a3afd9d51aea54
MolotkovD <damolotkov05@gmail.com>
2023-09-22 15:54:40 +0300

    Create Base Logic for binar file v2.

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,8 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         System.out.println("Start App!");
-//        CreateTable table = new CreateTable();
-//        table.SetName("TestDataBase");
-//        table.SetVersion("SL2.02");
-//        table.SetDate("21032023");
-
-        int[] array = new int[100];
-        int target = -5;
-        for (int i = 0; i < array.length; i++) {
-            array[i] = -i;
-        }
-        System.out.println(
-                Arrays.toString(array)
-        );
-        System.out.println(
-                Arrays.binarySearch(array, target)
-        );
-
 
 
         System.out.print(" ");
-//        table.SaveDataBase("");
     }
 }
